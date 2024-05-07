638370d2a5b0dca975d9598129598a7296a915a8
Xuan Yiming <XUANYIMING@OUTLOOK.COM.PE>
2024-05-06 23:28:04 -0500

    Update Solicitudes.jsx

--- src/pages/Solicitudes.jsx
+++ src/pages/Solicitudes.jsx
@@ -4,7 +4,10 @@ import Button from '@mui/material/Button';
 function Solicitudes () {
 
     return (
-        <><div className="row m-4">Solicitudes</div><Button variant="contained">Hello world</Button></>
+        <div>
+            <div className="row m-4">Solicitudes</div>
+            <Button variant="contained">Hello world</Button>
+        </div>
     )
 }
 
