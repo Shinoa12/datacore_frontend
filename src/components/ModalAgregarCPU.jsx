287f94d34f885a8cd648e5794968c69c53914bdd
Rodrigo Alonso <142762612+hifirod@users.noreply.github.com>
2024-05-09 16:53:49 -0500

    Refactor modal in Recursos

---
+++ src/components/ModalAgregarCPU.jsx
@@ -0,0 +1,114 @@
+import MuiDialog from "./common/MuiDialog";
+import Button from "@mui/material/Button";
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
+import DialogActions from "@mui/material/DialogActions";
+import InputAdornment from "@mui/material/InputAdornment";
+import IconButton from "@mui/material/IconButton";
+import MenuItem from "@mui/material/MenuItem";
+import CloseIcon from "@mui/icons-material/Close";
+import TextField from "@mui/material/TextField";
+
+function ModalAgregarCPU({ showModal, toggleModal }) {
+  const handleClose = () => {
+    toggleModal();
+  };
+
+  return (
+    <div>
+      <MuiDialog
+        open={showModal}
+        onClose={handleClose}
+        aria-labelledby="dialog-title"
+        fullWidth={true}
+      >
+        <DialogTitle
+          sx={{ m: 0, p: 2 }}
+          id="dialog-title"
+          style={{ color: "rgb(4, 35, 84)" }}
+        >
+          Agregar CPU
+        </DialogTitle>
+        <IconButton
+          aria-label="close"
+          onClick={handleClose}
+          sx={{
+            position: "absolute",
+            right: 8,
+            top: 12,
+            color: (theme) => theme.palette.grey[500],
+          }}
+        >
+          <CloseIcon />
+        </IconButton>
+        <DialogContent dividers>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="nombre"
+            label="Nombre"
+            type="text"
+            fullWidth
+            // value={nombre}
+            // onChange={manejarCambioNombre}
+          />
+          <TextField
+            margin="dense"
+            id="num_nucleos"
+            label="Número de núcleos"
+            type="number"
+            fullWidth
+            inputProps={{ min: 0 }}
+            // value={num_nucleos}
+            // onChange={manejarCambioNucleos}
+          />
+          <TextField
+            margin="dense"
+            id="frecuencia"
+            label="Frecuencia de reloj"
+            type="number"
+            fullWidth
+            inputProps={{ min: 0 }}
+            InputProps={{
+              endAdornment: <InputAdornment position="end">GHz</InputAdornment>,
+            }}
+            // value={frecuencia}
+            // onChange={manejarCambioFrecuencia}
+          />
+          <TextField
+            margin="dense"
+            id="ram"
+            label="RAM"
+            type="number"
+            fullWidth
+            inputProps={{ min: 0 }}
+            InputProps={{
+              endAdornment: <InputAdornment position="end">GB</InputAdornment>,
+            }}
+            // value={ram}
+            // onChange={manejarCambioRam}
+          />
+          <TextField
+            select
+            margin="dense"
+            id="estado"
+            label="Estado"
+            // value={estado}
+            // onChange={manejarCambioEstado}
+            fullWidth
+          >
+            <MenuItem value="enabled">Habilitado</MenuItem>
+            <MenuItem value="disabled">Deshabilitado</MenuItem>
+          </TextField>
+        </DialogContent>
+        <DialogActions sx={{ display: "flex", justifyContent: "end" }}>
+          <Button variant="contained" onClick={handleClose}>
+            CONFIRMAR
+          </Button>
+        </DialogActions>
+      </MuiDialog>
+    </div>
+  );
+}
+
+export default ModalAgregarCPU;
--- src/pages/Recursos.jsx
+++ src/pages/Recursos.jsx
@@ -1,16 +1,8 @@
-import * as React from "react";
+import { useState } from "react";
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
 import { DataGrid } from "@mui/x-data-grid";
-import MuiDialog from "../components/MuiDialog";
-import DialogTitle from "@mui/material/DialogTitle";
-import DialogContent from "@mui/material/DialogContent";
-import DialogActions from "@mui/material/DialogActions";
-import InputAdornment from "@mui/material/InputAdornment";
-import IconButton from "@mui/material/IconButton";
-import MenuItem from "@mui/material/MenuItem";
-import CloseIcon from "@mui/icons-material/Close";
-import TextField from "@mui/material/TextField";
+import ModalAgregarCPU from "../components/ModalAgregarCPU";
 
 const columns = [
   { field: "fecha_creacion", headerName: "Fecha de creación", width: 150 },
@@ -97,13 +89,10 @@ const rows = [
 ];
 
 function Recursos() {
-  const [open, setOpen] = React.useState(false);
+  const [showModal, setShowModal] = useState(false);
 
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-  const handleClose = () => {
-    setOpen(false);
+  const toggleModal = () => {
+    setShowModal(!showModal);
   };
 
   return (
@@ -126,108 +115,17 @@ function Recursos() {
             },
           }}
           pageSizeOptions={[5]}
-          //   checkboxSelection
           disableRowSelectionOnClick
         />
       </Box>
 
       <Box sx={{ display: "flex", justifyContent: "end", mt: 2 }}>
-        <Button variant="contained" onClick={handleClickOpen}>
+        <Button variant="contained" onClick={toggleModal}>
           AGREGAR
         </Button>
       </Box>
 
-      <MuiDialog
-        onClose={handleClose}
-        aria-labelledby="dialog-title"
-        open={open}
-        fullWidth={true}
-      >
-        <DialogTitle
-          sx={{ m: 0, p: 2 }}
-          id="dialog-title"
-          style={{ color: "rgb(4, 35, 84)" }}
-        >
-          Agregar CPU
-        </DialogTitle>
-        <IconButton
-          aria-label="close"
-          onClick={handleClose}
-          sx={{
-            position: "absolute",
-            right: 8,
-            top: 12,
-            color: (theme) => theme.palette.grey[500],
-          }}
-        >
-          <CloseIcon />
-        </IconButton>
-        <DialogContent dividers>
-          <TextField
-            autoFocus
-            margin="dense"
-            id="nombre"
-            label="Nombre"
-            type="text"
-            fullWidth
-            // value={nombre}
-            // onChange={manejarCambioNombre}
-          />
-          <TextField
-            margin="dense"
-            id="num_nucleos"
-            label="Número de núcleos"
-            type="number"
-            fullWidth
-            inputProps={{ min: 0 }}
-            // value={num_nucleos}
-            // onChange={manejarCambioNucleos}
-          />
-          <TextField
-            margin="dense"
-            id="frecuencia"
-            label="Frecuencia de reloj"
-            type="number"
-            fullWidth
-            inputProps={{ min: 0 }}
-            InputProps={{
-              endAdornment: <InputAdornment position="end">GHz</InputAdornment>,
-            }}
-            // value={frecuencia}
-            // onChange={manejarCambioFrecuencia}
-          />
-          <TextField
-            margin="dense"
-            id="ram"
-            label="RAM"
-            type="number"
-            fullWidth
-            inputProps={{ min: 0 }}
-            InputProps={{
-              endAdornment: <InputAdornment position="end">GB</InputAdornment>,
-            }}
-            // value={ram}
-            // onChange={manejarCambioRam}
-          />
-          <TextField
-            select
-            margin="dense"
-            id="estado"
-            label="Estado"
-            // value={estado}
-            // onChange={manejarCambioEstado}
-            fullWidth
-          >
-            <MenuItem value="enabled">Habilitado</MenuItem>
-            <MenuItem value="disabled">Deshabilitado</MenuItem>
-          </TextField>
-        </DialogContent>
-        <DialogActions sx={{ display: "flex", justifyContent: "end" }}>
-          <Button variant="contained" onClick={handleClose}>
-            CONFIRMAR
-          </Button>
-        </DialogActions>
-      </MuiDialog>
+      <ModalAgregarCPU showModal={showModal} toggleModal={toggleModal} />
     </div>
   );
 }
