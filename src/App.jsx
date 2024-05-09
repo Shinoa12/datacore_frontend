457191bbda00417807c28bf8840e8a3103fcf817
juandv182 <a20170089@pucp.edu.pe>
2024-05-09 17:28:01 -0500

    cambios login

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,65 @@
-import {BrowserRouter , Routes, Route, Navigate} from 'react-router-dom'
-import React, { useState } from "react";
-import Sidebar from './components/Navigate/SideBar';
-import Navbar from './components/Navigate/NavBar';
-import Recursos from './pages/Recursos'
-import Historial from './pages/Historial'
-import Home from './pages/Home'
-import './App.css'
-
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import React, { useContext, useEffect, useState } from "react";
+import './App.css';
+import './normalize.css';
+import { LoginPage } from './auth/LoginPage';
+import { AuthContext } from './auth/context/AuthContext';
+import { AppRoutes } from './routes/AppRoutes';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import UserContext from './context/UserContext';
 
 function App() {
-  const [sidebarToggle, setSidebarToggle] = useState(false);
+  const { login } = useContext(AuthContext);
+  const clientId = "722348533329-fcvbgk9bl8qerclkpoav4quk9gcsfbnl.apps.googleusercontent.com";  
+  const [userInfo, setUserInfo] = useState([]);
+
+  const verifyToken = () => {
+    const access_key = localStorage.getItem("access_token");
+    const username = localStorage.getItem("username");
+
+    fetch('/user/token/verify/', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ token: access_key })
+    })
+    .then((response) => {
+      if (response.ok) {
+        setUserInfo({ ...userInfo, access_token: access_key, username: username });
+      } else {
+        setUserInfo({ ...userInfo, access_token: null, username: null });
+      }
+    });
+  };
+
+  useEffect(() => {
+    verifyToken();
+  }, []);
+
+  const updateUserInfo = (value) => {
+    setUserInfo(value);
+  };
 
   return (
-    <div>
-      <BrowserRouter>
-        <div className="flex">
-          
-          <Sidebar sidebarToggle={sidebarToggle} />
-          <Navbar sidebarToggle={sidebarToggle} setSidebarToggle={setSidebarToggle} />
-          
-        </div>
-
-        <div className={`${sidebarToggle ? "" : "ml-64"}`}>
+    <UserContext.Provider value={{ userInfo, updateUserInfo }}>
+      <GoogleOAuthProvider clientId={clientId}>
+        <div>
           <Routes>
-            <Route path="/" element={<Navigate to="/Home" />} />
-            <Route path="/Home" element={<Home />} />
-            <Route path="/Recursos" element={<Recursos />} />
-            <Route path="/Historial" element={<Historial />} />
+            {
+              login.isAuth
+              ? (<>
+                  <Route path='/*' element={<AppRoutes />} />
+                </>)
+              : (<>
+                  <Route path='/login' element={<LoginPage />} />
+                  <Route path='/*' element={<Navigate to="/login" />} />
+                </>)
+            }
           </Routes>
         </div>
-      </BrowserRouter>
-    </div>
+      </GoogleOAuthProvider>
+    </UserContext.Provider>
   );
 }
 
