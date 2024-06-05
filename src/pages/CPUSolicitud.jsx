9748adf61b7f0268909056662e4802aa463d91f9
sunjis <sun.ji@pucp.edu.pe>
2024-06-04 22:38:24 -0500

    Modal de librerías
    Modal de librerías al 50%

--- src/pages/CPUSolicitud.jsx
+++ src/pages/CPUSolicitud.jsx
@@ -5,6 +5,8 @@ import { Link } from 'react-router-dom';
 import ModalSolicitudExito from '../components/ModalSolicitudExito';
 import CPUDropdown from '../components/CPUDropdown';
 import pdfGuia from '../img/Manual de usuario para investigador.pdf';
+import { FormControl, InputLabel, MenuItem, Select } from '@mui/material';
+
 
 
 
@@ -13,6 +15,17 @@ function CPUSolicitud() {
     const [showDropMessage, setShowDropMessage] = useState(true);
     const [executionParameters, setExecutionParameters] = useState('');
     const [showModal, setShowModal] = useState(false); 
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [selectedOption, setSelectedOption] = useState('');
+
+
+    const openModal = () => {
+        setIsModalOpen(true);
+    };
+
+    const closeModal = () => {
+        setIsModalOpen(false);
+    };
 
     const handleFileChange = (event) => {
         setSelectedFiles(prevFiles => [...prevFiles, ...Array.from(event.target.files)]);
@@ -47,6 +60,10 @@ function CPUSolicitud() {
         setShowModal(true);
     };
 
+    const handleModalOptionChange = (event) => {
+        setSelectedOption(event.target.value);
+    };
+
     const [selectedCPU, setSelectedCPU] = useState("");
 
     const handleCPUChange = (cpu) => { 
@@ -79,11 +96,38 @@ function CPUSolicitud() {
                         <p style={{ marginBottom: '10px' }}><strong>Tamaño de memoria RAM: {selectedCPU.tamaño_ram}</strong></p>
                         {/* <p style={{ marginBottom: '10px' }}><strong>Sistema Operativo:</strong></p> */}
                         <div style={{ textAlign: 'center' }}>
-                            <Link to="/">
-                                <button style={{ padding: '10px 20px', fontSize: '16px', borderRadius: '5px', backgroundColor: '#162447', color: '#fff', border: 'none', cursor: 'pointer' }}>
-                                    Descubre más sobre el recurso
-                                </button>
-                            </Link>
+                            <button onClick={openModal} style={{ padding: '10px 20px', fontSize: '16px', borderRadius: '5px', backgroundColor: '#162447', color: '#fff', border: 'none', cursor: 'pointer' }} >
+                                Descubre más sobre el recurso
+                            </button>
+                            {isModalOpen && (
+                                <div style={{ position: 'fixed', top: '0', left: '0', width: '100%', height: '100%', backgroundColor: 'rgba(0,0,0,0.5)', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                                    <div style={{ position: 'relative', zIndex: '1000', backgroundColor: '#fff', padding: '20px', borderRadius: '10px', width: '90%', maxWidth: '1000px', height: '80%', maxHeight: '800px', textAlign: 'left' }} >
+                                        <h2 style={{ color: "rgb(4, 35, 84)" }} className="font-bold text-3xl mb-4">Librerías</h2>
+                                        <span style={{ position: 'absolute', top: '20px', right: '20px', cursor: 'pointer' }} onClick={closeModal}>
+                                            <FontAwesomeIcon icon={faTimes} style={{ fontSize: '24px' }}/>
+                                        </span>
+                                        <FormControl required sx={{ minWidth: 240, width: '100%' }}>
+                                            <InputLabel id="modal-herramientas-label">Herramientas</InputLabel>
+                                            <Select
+                                                labelId="modal-herramientas-label"
+                                                id="modal-herramientas"
+                                                value={selectedOption}
+                                                onChange={handleModalOptionChange}
+                                                label="Herramientas"
+                                                style={{ width: '50%', marginTop: '20px' }}
+                                            >
+                                                <MenuItem value="">Seleccionar opción</MenuItem>
+                                                <MenuItem value="opcion1">Opción 1</MenuItem>
+                                                <MenuItem value="opcion2">Opción 2</MenuItem>
+                                                <MenuItem value="opcion3">Opción 3</MenuItem>
+                                            </Select>
+                                        </FormControl>
+                                        <div style={{ position: 'absolute', bottom: '20px', left: '20px' }}>
+                                            <strong style={{ fontSize: '20px', marginBottom: '10px', color: "rgb(4, 35, 84)" }}>Si NO encontraste tu librería aquí dirígete al icono de ayuda en la sección anterior y comunícate con nuestro equipo.</strong>
+                                        </div>
+                                    </div>
+                                </div>
+                            )}
                         </div>
                     </div>
                     <br></br>
@@ -92,14 +136,7 @@ function CPUSolicitud() {
                             Posición en cola del recurso
                         </strong>
                     </h2>
-                    <div style={{
-                        position: 'relative',
-                        backgroundColor: 'rgba(0, 0, 0, 0.08)',
-                        padding: '10px',
-                        borderRadius: '5px',
-                        textAlign: 'left',
-                        minWidth: '30px',
-                    }}>
+                    <div style={{ backgroundColor: 'rgba(0, 0, 0, 0.08)', padding: '20px', borderRadius: '5px', marginTop: '20px'}}>
                         <span style={{ color: "rgb(4, 35, 84)" }}>3</span>
                     </div>
                 </div>
@@ -109,20 +146,11 @@ function CPUSolicitud() {
                             Archivos
                         </strong>
                     </h2>
-                    <div
-                        style={{ border: '2px dashed #ccc', borderRadius: '5px', padding: '20px', textAlign: 'center', cursor: 'pointer', position: 'relative', height: '200px' }}
-                        onDrop={handleDrop}
-                        onDragOver={handleDragOver}
-                    >
+                    <div style={{ border: '2px dashed #ccc', borderRadius: '5px', padding: '20px', textAlign: 'center', cursor: 'pointer', position: 'static', height: '200px' }} onDrop={handleDrop} onDragOver={handleDragOver} >
                         {!selectedFiles.length && (
                             <label style={{ color: 'blue', textDecoration: 'underline', cursor: 'pointer' }}>
                                 Haz clic para elegir un archivo
-                                <input
-                                    type="file"
-                                    style={{ display: 'none' }}
-                                    onChange={handleFileChange}
-                                    multiple
-                                />
+                                <input type="file" style={{ display: 'none' }} onChange={handleFileChange} multiple />
                             </label>
                         )}
                         {!selectedFiles.length && showDropMessage && <span> o arrastra y suelta archivos aquí</span>}
@@ -145,13 +173,7 @@ function CPUSolicitud() {
                             Parámetros de ejecución
                         </strong>
                     </h2>
-                    <input
-                        type="text"
-                        value={executionParameters}
-                        onChange={handleExecutionParametersChange}
-                        placeholder="Agregar parámetros..."
-                        style={{ width: '100%', height: '100px', fontSize: '16px', fontStyle: 'italic', textAlign: 'left', paddingLeft: '20px', t: '20px', paddingTop: '20px', paddingBottom: '20px', backgroundColor: 'rgba(0, 0, 0, 0.08)', border: 'none', borderRadius: '5px', boxSizing: 'border-box' }}
-                    />
+                    <input type="text" value={executionParameters} onChange={handleExecutionParametersChange} placeholder="Agregar parámetros..." style={{ width: '100%', height: '100px', fontSize: '16px', fontStyle: 'italic', textAlign: 'left', paddingLeft: '20px', t: '20px', paddingTop: '20px', paddingBottom: '20px', backgroundColor: 'rgba(0, 0, 0, 0.08)', border: 'none', borderRadius: '5px', boxSizing: 'border-box' }} />
                     <h2 style={{ fontSize: '15px', marginTop: '20px', marginBottom: '10px', color: "rgb(4, 35, 84)" }}>
                         <strong>
                             Si aún no entiendes la pantalla o cómo crear tu solicitud, ¡NO TE PREOCUPES! <br></br>
