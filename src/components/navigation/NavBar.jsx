673001c839f79a3e75610ca1fd8b7ca55a8284c2
Rodrigo Alonso <142762612+hifirod@users.noreply.github.com>
2024-06-16 05:28:31 -0500

    Refactor navbar and sidebar

--- src/components/navigation/NavBar.jsx
+++ src/components/navigation/NavBar.jsx
@@ -7,55 +7,49 @@ import MenuIcon from "@mui/icons-material/Menu";
 import LogoutIcon from "@mui/icons-material/Logout";
 import HelpIcon from "@mui/icons-material/Help";
 
+const iconStyles = { color: "white", fontSize: "1.75rem" };
+export const navbarHeight = "110px";
+
 function Navbar({ sidebarToggle, setSidebarToggle }) {
   const theme = useTheme();
   const { handlerLogout } = useContext(AuthContext);
 
   return (
     <div
-      className={`fixed top-0 right-0 z-10 w-full transition-all duration-300`}
-      style={{
-        width: sidebarToggle ? "100%" : "calc(100% - 16rem)", // 16rem (64 * 0.25rem) es el ancho de la sidebar
-        backgroundColor: theme.palette.primary.main,
-      }}
+      className={`z-10 transition-all duration-300 w-full ${
+        sidebarToggle ? "" : "ml-64"
+      }`}
     >
       <nav
-        className="px-6 py-6 flex justify-between"
+        className="p-6 flex justify-between"
+        style={{
+          backgroundColor: theme.palette.primary.main,
+          height: navbarHeight,
+        }}
       >
-        <div className="flex flex-row items-center">
+        {/* Menú y logo */}
+        <div className="flex items-center gap-7">
           <IconButton onClick={() => setSidebarToggle(!sidebarToggle)}>
-            <MenuIcon
-              sx={{
-                color: "white",
-                fontSize: "1.75rem",
-              }}
-            />
+            <MenuIcon sx={iconStyles} />
           </IconButton>
 
           <Link to="/">
             <img
               src="/src/assets/datacore_logo.svg"
               alt="DataCore"
-              style={{ height: "60px", marginLeft: "1.75rem" }}
+              style={{ height: "60px" }}
             />
           </Link>
         </div>
-        <div className="flex items-center gap-6">
+
+        {/* Ayuda y cerrar sesión */}
+        <div className="flex items-center gap-7">
           <IconButton component={Link} to="/ayuda">
-            <HelpIcon
-              sx={{
-                color: "white",
-                fontSize: "1.75rem",
-              }}
-            />
+            <HelpIcon sx={iconStyles} />
           </IconButton>
+
           <IconButton onClick={handlerLogout}>
-            <LogoutIcon
-              sx={{
-                color: "white",
-                fontSize: "1.75rem",
-              }}
-            />
+            <LogoutIcon sx={iconStyles} />
           </IconButton>
         </div>
       </nav>
--- src/components/navigation/SideBar.jsx
+++ src/components/navigation/SideBar.jsx
@@ -9,121 +9,123 @@ import {
 } from "react-icons/fa";
 import { BsCpuFill } from "react-icons/bs";
 import { Link } from "react-router-dom";
+import { useTheme } from "@mui/material/styles";
+import Button from "@mui/material/Button";
+import { navbarHeight } from "./NavBar";
 
-const Sidebar = ({ sidebarToggle }) => {
+const sidebarButtonStyles = {
+  width: "100%",
+  borderRadius: "0px",
+  justifyContent: "flex-start",
+  paddingTop: "0.75rem",
+  paddingBottom: "0.75rem",
+};
+
+const adminModules = [
+  {
+    name: "Usuarios autorizados",
+    path: "/usuarios-autorizados",
+    icon: <FaUserShield />,
+  },
+  {
+    name: "Usuarios desautorizados",
+    path: "/usuarios-desautorizados",
+    icon: <FaUserSlash />,
+  },
+  {
+    name: "Historial",
+    path: "/historial",
+    icon: <FaHistory />,
+  },
+  {
+    name: "Recursos",
+    path: "/recursos",
+    icon: <BsCpuFill />,
+  },
+  {
+    name: "Ajustes",
+    path: "/ajustes",
+    icon: <FaCog />,
+  },
+];
+
+const userModules = [
+  {
+    name: "Home",
+    path: "/home",
+    icon: <FaHome />,
+  },
+  {
+    name: "Solicitudes",
+    path: "/solicitudes",
+    icon: <FaInbox />,
+  },
+];
+
+function Sidebar({ sidebarToggle }) {
+  const theme = useTheme();
+  const isAdmin = localStorage.getItem("is_admin") === "true";
+  const modules = isAdmin ? adminModules : userModules;
+
   return (
     <div
-      className={`${sidebarToggle ? "hidden" : "block"} w-64 fixed h-full text-center flex flex-col`}
-      
+      className={`${
+        sidebarToggle ? "hidden" : "block"
+      } w-64 fixed h-full flex flex-col`}
     >
-      <div className="text-center px-4 py-2">
+      {/* Información del usuario */}
+      <div
+        className="flex flex-col flex-none items-center justify-center px-4 py-2 h-full"
+        style={{ height: navbarHeight }}
+      >
         <FaRegUserCircle
-          size={39}
-          className="inline-block text-dark font-semibold"
+          size={40}
+          className="text-gray-800"
+          style={{ color: theme.palette.primary.main }}
         ></FaRegUserCircle>
-        <h4 className="text-dark">
+
+        <p
+          className="text-gray-800 font-semibold"
+          style={{ color: theme.palette.primary.main }}
+        >
           {localStorage.getItem("first_name") +
             " " +
             localStorage.getItem("last_name")}
-        </h4>
+        </p>
+
         <p className="text-gray-400">
-          {localStorage.getItem("is_admin") === "true"
-            ? "(Administrador)"
-            : "(Usuario)"}
+          {isAdmin ? "(Administrador)" : "(Usuario)"}
         </p>
       </div>
 
-      <div className="flex-grow">
-        <ul className="mt-1 text-white font-semibold" style={{ backgroundColor: "rgba(4, 35, 84, 1)" }}>
-          {localStorage.getItem("is_admin") === "true" ? (
-            <>
-              <Link to="usuarios-autorizados" >
-                <li className=" hover:shadow hover:bg-blue-500 py-2 text-center">
-                  <button className="px-3 text-sm">
-                    <FaUserShield className="inline-block w-6 h-5 mr-3 -mt-2"></FaUserShield>
-                    Usuarios autorizados
-                  </button>
-                </li>
-              </Link>
-              <hr className="border-4 border-white" />
-              <Link to="usuarios-desautorizados">
-                <li className=" hover:shadow hover:bg-blue-500 py-2 text-center">
-                  <button className="px-3 text-sm">
-                    <FaUserSlash className="inline-block w-6 h-5 mr-3 -mt-2"></FaUserSlash>
-                    Usuarios desautorizados
-                  </button>
-                </li>
-              </Link>
-              <hr className="border-4 border-white" />
-              <Link to="historial">
-                <li className="hover:shadow hover:bg-blue-500 py-2 text-center">
-                  <button className="px-3 text-sm">
-                    <FaHistory className="inline-block w-6 h-5 mr-3 -mt-2"></FaHistory>
-                    Historial
-                  </button>
-                </li>
-              </Link>
-              <hr className="border-4 border-white" />
-              <Link to="recursos">
-                <li className=" hover:shadow hover:bg-blue-500 py-2 text-center">
-                  <button className="px-3 text-sm">
-                    <BsCpuFill className="inline-block w-6 h-5 mr-3 -mt-2"></BsCpuFill>
-                    Recursos
-                  </button>
-                </li>
-              </Link>
-              <hr className="border-4 border-white" />
-              <Link to="ajustes">
-                <li className=" hover:shadow hover:bg-blue-500 py-2 text-center">
-                  <button className="px-3 text-sm">
-                    <FaCog className="inline-block w-6 h-6 mr-3 -mt-2"></FaCog>
-                    Ajustes
-                  </button>
-                </li>
-              </Link>
-              <hr className="border-4 border-white" />
-            </>
-          ) : (
-            <>
-              <Link to="home">
-                <li className=" hover:shadow hover:bg-blue-500 py-2 text-center">
-                  <button className="px-3 text-sm">
-                    <FaHome className="inline-block w-6 h-5 mr-3 -mt-2"></FaHome>
-                    Home
-                  </button>
-                </li>
-              </Link>
-              <hr className="border-4 border-white" />
-
-              <Link to="solicitudes">
-                <li className=" hover:shadow hover:bg-blue-500 py-2 text-center">
-                  <button className="px-3 text-sm">
-                    <FaInbox className="inline-block w-6 h-6 mr-3 -mt-2"></FaInbox>
-                    Solicitudes
-                  </button>
-                </li>
-              </Link>
-            </>
-          )}
-        </ul>
+      {/* Módulos */}
+      <div className="flex flex-col grow gap-3">
+        {modules.map((module, index) => (
+          <Button
+            key={index}
+            component={Link}
+            to={module.path}
+            variant="contained"
+            disableElevation
+            size="small"
+            startIcon={module.icon}
+            sx={sidebarButtonStyles}
+          >
+            {module.name}
+          </Button>
+        ))}
       </div>
-      
-      <div className="mt-auto text-center py-4 bg-white flex justify-center items-center">
-        <img
-          src="/src/assets/Imagen DataCore.png"
-          alt="DataCore"
-          className="mx-auto mb-2"
-          style={{ maxWidth: "80px", height: "auto" }}
-        />
+
+      {/* Logo */}
+      <div className="py-4 flex justify-center items-center">
         <img
           src="/src/assets/pucp_logo.png"
           alt="PUCP"
-          className="mx-auto mb-2"
-          style={{ maxWidth: "130px", height: "auto" }}
+          style={{ maxWidth: "180px", height: "auto" }}
         />
       </div>
     </div>
   );
-};
+}
 
 export default Sidebar;
