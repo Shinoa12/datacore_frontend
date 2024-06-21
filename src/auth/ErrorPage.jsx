44e635f654084a3814c22439a6a0067cd2c6427f
Rodrigo Alonso <142762612+hifirod@users.noreply.github.com>
2024-06-21 16:59:18 -0500

    Update error page

--- src/auth/ErrorPage.jsx
+++ src/auth/ErrorPage.jsx
@@ -1,26 +1,39 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import warningImg from '../assets/warning.png';
+import { Link } from "react-router-dom";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import WarningIcon from "@mui/icons-material/Warning";
 
 const ErrorPage = () => {
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
       <div className="bg-white rounded-lg shadow-lg overflow-hidden w-full max-w-lg">
-        <div className="flex flex-col items-center p-6">
-          <img src={warningImg} alt="Advertencia" className="w-12 h-12 mb-4" />
-          <h5 className="text-lg font-bold mb-2">Advertencia</h5>
+        <div className="flex flex-col items-center px-6 py-8 gap-3">
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              gap: "0.5rem",
+              color: "#B9333A",
+              mb: 2,
+            }}
+          >
+            <WarningIcon sx={{ fontSize: "5rem" }} />
+            <p className="text-2xl font-bold">Advertencia</p>
+          </Box>
           <p className="text-center mb-2">
             Ups, parece que has llegado a una zona exclusiva.
           </p>
           <p className="text-center mb-2">
-            Si necesitas acceder o tienes alguna pregunta, no dudes en contactarte con nuestro equipo en el DAI a través del siguiente correo: 
-            <span className="font-semibold"> correo@pucp.edu.pe </span>
-            y estaremos encantados de asistirte.
+            Si necesitas acceder o tienes alguna pregunta, no dudes en
+            contactarte con nuestro equipo en el DAI al correo
+            <span className="font-semibold"> correo@pucp.edu.pe </span>y
+            estaremos encantados de ayudarte.
           </p>
           <p className="text-center mb-4">¡Gracias por tu comprensión!</p>
-          <Link to="/" className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+          <Button component={Link} to="/" variant="contained">
             Cerrar
-          </Link>
+          </Button>
         </div>
       </div>
     </div>
